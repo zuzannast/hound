0bddb858cbc60e5adc54c90b06de88ac1592dab8
Joel Oliveira <joel.oliveira@gmail.com>
2017-01-25 14:15:16 -0500

    Hound

--- spec/models/linter/reek_spec.rb
+++ spec/models/linter/reek_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe Linter::Reek do
   it_behaves_like "a linter" do
-    let(:linter_name) { 'reek' }
+    let(:linter_name) { "reek" }
     let(:lintable_files) { %w(foo.rb foo.rake) }
     let(:not_lintable_files) { %w(foo.js) }
   end
