0cf954b73fb0bc80cfad3bff4d5935eb4f55f1d0
Joel Oliveira <joel.oliveira@gmail.com>
2017-01-25 14:13:18 -0500

    Pull common tests into shared example(s)

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -16,4 +16,43 @@ RSpec.shared_examples "a linter" do
       end
     end
   end
+
+  describe "#file_review" do
+    it "is a new file review" do
+      repo = instance_double("Repo", owner: nil)
+      build = instance_double(
+        "Build",
+        commit_sha: "somesha",
+        pull_request_number: 123,
+        repo: repo,
+      )
+      filename = lintable_files.first
+      commit_file = instance_double(
+        "CommitFile",
+        content: "code",
+        filename: filename,
+        patch: "patch",
+      )
+      file_review = instance_double("FileReview")
+      hound_config = instance_double("HoundConfig")
+      missing_owner = instance_double("MissingOwner")
+      allow(FileReview).to receive(:create!).and_return(file_review)
+      allow(MissingOwner).to receive(:new).and_return(missing_owner)
+      allow(Resque).to receive(:enqueue)
+      linter = described_class.new(build: build, hound_config: hound_config)
+
+      expect(linter.file_review(commit_file)).to eq file_review
+      expect(Resque).to have_received(:enqueue)
+    end
+  end
+
+  describe "#name" do
+    it "is the class name converted to a config-friendly format" do
+      build = instance_double("Build")
+      hound_config = instance_double("HoundConfig")
+      linter = described_class.new(build: build, hound_config: hound_config)
+
+      expect(linter.name).to eq linter_name
+    end
+  end
 end
